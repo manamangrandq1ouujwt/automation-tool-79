439409697b8fbe9808580fb2c53fdeb16144228c
manamangrandq1ouujwt <hayehundaf@gmail.com>
2026-08-19 15:14:50 +0300

    Improve logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,31 +1,15 @@
 import logging
+import logging.handlers
 
-
-def setup_logger(name):
-    logger = logging.getLogger(name)
-    if not logger.hasHandlers():
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.setLevel(logging.INFO)
+def setup_logger(log_file='app.log', log_level=logging.INFO):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level)
+    handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=5 * 1024 * 1024, backupCount=3)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     return logger
 
-
-def log_info(logger, message):
-    logger.info(message)
-
-
-def log_warning(logger, message):
-    logger.warning(message)
-
-
-def log_error(logger, message):
-    logger.error(message)
-
-
 if __name__ == '__main__':
-    test_logger = setup_logger('TestLogger')
-    log_info(test_logger, 'This is an info message.')
-    log_warning(test_logger, 'This is a warning message.')
-    log_error(test_logger, 'This is an error message.')
+    log = setup_logger()
+    log.info('Logger setup complete.')
